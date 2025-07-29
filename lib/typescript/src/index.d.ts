c4fcb1df07f070f034fed1c05d27da932075872b
Bogdan Boksan <bogdan.boksan@ananas.rs>
2025-07-29 21:47:18 +0200

    Include built lib directory for package distribution
    - Comment out lib/ from .gitignore to include built files
    - Add compiled TypeScript and module files for Metro bundler

---
+++ lib/typescript/src/index.d.ts
@@ -0,0 +1,3 @@
+export declare function initialize(developmentMode: boolean): void;
+export declare function encrypt(pan: string, cardHolderName: string, expiryYear: number, expiryMonth: number, cvv: string, nonce: string): Promise<string>;
+//# sourceMappingURL=index.d.ts.map
