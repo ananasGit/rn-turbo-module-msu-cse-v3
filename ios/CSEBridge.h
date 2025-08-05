6716bb64fa4e6e1e300782eecf5b841c37151d52
Bogdan Boksan <bogdan.boksan@ananas.rs>
2025-08-05 16:52:45 +0200

    Fixed bridging header

--- ios/CSEBridge.h
+++ ios/CSEBridge.h
@@ -2,10 +2,23 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@class CSEBridge;
 @class CSEEncryptResult;
 @class CSEEncryptError;
 
 typedef void (^CSEEncryptCallback)(CSEEncryptResult *result);
+
+@interface CSEBridge : NSObject
+
+- (instancetype)initWithDevelopmentMode:(BOOL)developmentMode;
+
+- (void)encryptWithPan:(NSString *)pan
+        cardHolderName:(NSString *)cardHolderName
+            expiryYear:(NSInteger)expiryYear
+           expiryMonth:(NSInteger)expiryMonth
+                   cvv:(NSString *)cvv
+                 nonce:(NSString *)nonce
+              callback:(CSEEncryptCallback)callback;
+
+@end
 
 NS_ASSUME_NONNULL_END
