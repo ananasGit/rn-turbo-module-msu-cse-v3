4d2542fd9b5d1b333fd78a14c75ae449de6fed5d
Bogdan Boksan <bogdan.boksan@ananas.rs>
2025-08-12 15:43:59 +0200

    Enhance CSE module by adding new validation methods for CVV, card holder name, and card token

--- ios/RnTurboModuleMsuCseV3.h
+++ ios/RnTurboModuleMsuCseV3.h
@@ -1,5 +1,4 @@
 #import <RnTurboModuleMsuCseV3Spec/RnTurboModuleMsuCseV3Spec.h>
-#import "CSEBridge.h"
 
 @interface RnTurboModuleMsuCseV3 : NSObject <NativeRnTurboModuleMsuCseV3Spec>
 
--- lib/typescript/src/NativeRnTurboModuleMsuCseV3Spec.d.ts
+++ lib/typescript/src/NativeRnTurboModuleMsuCseV3Spec.d.ts
@@ -2,10 +2,15 @@ import type { TurboModule } from 'react-native';
 export interface Spec extends TurboModule {
     initialize(developmentMode: boolean): void;
     encrypt(pan: string, cardHolderName: string, expiryYear: number, expiryMonth: number, cvv: string, nonce: string): Promise<string>;
+    encryptCVV(cvv: string, nonce: string): Promise<string>;
     isValidPan(pan: string): Promise<boolean>;
     isValidCVV(cvv: string, pan?: string): Promise<boolean>;
+    isValidCardHolderName(name: string): Promise<boolean>;
+    isValidCardToken(token: string): Promise<boolean>;
     isValidExpiry(month: number, year: number): Promise<boolean>;
     detectBrand(pan: string): Promise<string>;
+    getErrors(): Promise<string[]>;
+    hasErrors(): Promise<boolean>;
 }
 declare const _default: Spec;
 export default _default;
